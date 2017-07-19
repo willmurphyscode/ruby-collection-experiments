e1e312e46cf8bb5a4fff1f06671dfd7cbddb060f
Will Murphy <willmurphyscode@gmail.com>
2017-07-19 08:14:48 -0400

    add symbol key test

--- test/case_insensitive_hash_test.rb
+++ test/case_insensitive_hash_test.rb
@@ -10,5 +10,13 @@ class CaseInsensitiveHashTest < Minitest::Test
       sut[lower_key] = value
       assert_equal sut[upper_key], value, 'failed to fetch by upcased key'
     end
+
+    it 'fetches via symbol' do 
+      lower_key = 'foo'
+      symbol_key = :foo
+      value = 'test data'
+      sut[lower_key] = value
+      assert_equal sut[symbol_key], value, 'failed to fetch by symbol key'
+    end
   end
 end
