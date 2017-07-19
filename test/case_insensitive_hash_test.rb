91ff01407f75a2b044d8f34b2e4f3d61e54c44e3
Will Murphy <willmurphyscode@gmail.com>
2017-07-19 08:31:00 -0400

    add more tests

--- test/case_insensitive_hash_test.rb
+++ test/case_insensitive_hash_test.rb
@@ -3,20 +3,62 @@ require 'test_helper'
 class CaseInsensitiveHashTest < Minitest::Test
   describe '[] and []=' do
     let(:sut) { CaseInsensitiveHash.new }
+
+    def altered_key_matches(original_key, altered_key, msg)
+      expected = sut[original_key]
+      actual = sut[altered_key]
+      assert_equal expected, actual, msg
+    end
+
+    def can_retrieve_by_altered_key(original_key:, altered_key:, value:)
+      can_retrieve_by_altered_key_impl(
+        original_key: original_key,
+        altered_key: altered_key,
+        value: value
+      )
+      can_retrieve_by_altered_key_impl(
+        original_key: altered_key,
+        altered_key: original_key,
+        value: value
+      )
+    end
+
+    def can_retrieve_by_altered_key_impl(original_key:, altered_key:, value:)
+      sut[original_key] = value
+      msg = "Key set by #{original_key} cannot be retrieved by #{altered_key}"
+      altered_key_matches(original_key, altered_key, msg)
+    end
+
     it 'fetches via upcased keys' do
-      lower_key = 'foo'
-      upper_key = 'FOO'
-      value = 'test data'
-      sut[lower_key] = value
-      assert_equal sut[upper_key], value, 'failed to fetch by upcased key'
+      can_retrieve_by_altered_key(
+        original_key: 'foo',
+        altered_key: 'FOO',
+        value: 'some value'
+      )
     end
 
     it 'fetches via symbol' do 
-      lower_key = 'foo'
-      symbol_key = :foo
-      value = 'test data'
-      sut[lower_key] = value
-      assert_equal sut[symbol_key], value, 'failed to fetch by symbol key'
+      can_retrieve_by_altered_key(
+        original_key: 'foo',
+        altered_key: :foo,
+        value: 'some value'
+      )
+    end
+
+    it 'fetches via downcased key' do
+      can_retrieve_by_altered_key(
+        original_key: 'FOO',
+        altered_key: 'foo',
+        value: 'some value'
+      )
+    end
+
+    it 'fetches via miscased symbol' do
+      can_retrieve_by_altered_key(
+        original_key: 'FOO',
+        altered_key: :foo,
+        value: 'some value'
+      )
     end
   end
 end
