36dc397a8a58bc7320db3dd081f996e813309a04
Will Murphy <willmurphyscode@gmail.com>
2017-07-19 08:35:19 -0400

    added validations on key

--- lib/collections/case_insensitive_hash.rb
+++ lib/collections/case_insensitive_hash.rb
@@ -4,11 +4,13 @@ class CaseInsensitiveHash
   end
 
   def fetch_insensitive(key)
+    raise ArgumentError unless key.respond_to?(:to_s)    
     @hsh[key.to_s.downcase.to_sym]
   end
 
   def set(key, value)
-    actual_key = key.downcase.to_sym
+    raise ArgumentError unless key.respond_to?(:to_s)
+    actual_key = key.to_s.downcase.to_sym
     @hsh[actual_key] = value
   end
 
